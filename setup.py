f8ddb338d3f472280ccd05cd732b4359b60845c5
Eric Appelt <eric.appelt@gmail.com>
2019-02-12 21:00:33 -0500

    Add python package skeleton

---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = 'slurmlint'
+DESCRIPTION = 'Linter for Slurm configuration files'
+URL = 'https://github.com/appeltel/slurmlint'
+EMAIL = 'eric.appelt@gmail.com'
+AUTHOR = 'Eric Appelt'
+
+REQUIRED = []
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+    exec(f.read(), about)
+
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    entry_points={
+        'console_scripts': [],
+    },
+    install_requires=REQUIRED,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: System Administrators',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: System :: Systems Administration',
+    ],
+)
---
+++ slurmlint/__version__.py
@@ -0,0 +1,3 @@
+__version__ = '0.0.0'
+
+__title__ = 'Linter for Slurm config files'
