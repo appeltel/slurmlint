f941e825224446f9be7550e44b11ab146f64041c
Eric Appelt <eric.appelt@gmail.com>
2019-03-07 14:24:08 -0600

    Add expand hostlist function

--- slurmlint/hosts.py
+++ slurmlint/hosts.py
@@ -5,6 +5,90 @@ import re
 
 
 RE_ISNUM = re.compile('[0-9]+')
+RE_NEXTBRACKET = re.compile('([^[]*)\[([^]]*)\](.*)')
+
+
+def expand_hostlist(hostlist, max_depth=20):
+    """
+    Given a slurm-style list of hosts, expand and
+    return individual hostnames.
+
+    :param str hostlist: slurm-style host list
+    :param int max_depth: maximum number of brackets in an entry
+    :returns: list of hostnames
+    :rtype: list(str)
+    """
+    result = _split_outside_brackets(hostlist, ',')
+    depth = 0
+    while depth < max_depth:
+        if not any(host.endswith(']') for host in result):
+            return result
+        depth += 1
+
+        new_result = []
+        for item in result:
+            if not item.endswith(']'):
+                new_result.append(item)
+            else:
+                new_result.extend(_expand_next_bracket(item))
+
+        result = new_result
+
+    raise ValueError('Too many brackets') 
+
+
+def _split_outside_brackets(raw, splitchar):
+    """
+    Split a string on a given character only when the
+    character is outside brackets
+
+    Nested brackets are right out
+    """
+    result = []
+    word = ''
+    in_brackets = False
+    for char in raw:
+        if char == '[' and not in_brackets:
+            word = word + char
+            in_brackets = True
+        elif char == '[' and in_brackets:
+            raise ValueError('Nested brackets are right out')
+        elif char == ']' and in_brackets:
+            word = word + char
+            in_brackets = False
+        elif char == ']' and not in_brackets:
+            raise ValueError('Unmatched end-bracket')
+        elif not in_brackets and char == splitchar and word:
+            result.append(word)
+            word = ''
+        elif not in_brackets and char == splitchar and not word:
+            raise ValueError('Missing item between separator')
+        else:
+            word = word + char
+    if not word:
+        raise ValueError('Missing item after separator')
+        
+    result.append(word)
+    return result
+
+
+def _expand_next_bracket(hostlist):
+    """
+    Take a hostlist and expand the next set of brackets
+    returning a list of either hosts or hostlists if
+    there are multiple brackets.
+    """
+    if not hostlist.endswith(']'):
+        raise ValueError(
+            'Invalid host list, not ending in bracket'
+        )
+    match = RE_NEXTBRACKET.match(hostlist)
+    if not match:
+        raise ValueError('Invalid brackets in host list')
+    prefix = match.group(1)
+    numlist = match.group(2)
+    suffix = match.group(3)
+    return [prefix + num + suffix for num in expand_numlist(numlist)] 
 
 
 def expand_numlist(raw):
--- tests/test_hosts.py
+++ tests/test_hosts.py
@@ -12,3 +12,12 @@ def test_expand_numlist():
 
     expected = ['5', '123', '5017', '5018', '5019']
     assert hosts.expand_numlist('5,123,5017-5019') == expected
+
+
+def test_expand_hostlist():
+    expected = [
+        'ng032', 'cn304', 'cn305', 'cn306', 'cn308', 'gpu0012', 'gpu0013',
+        'gpu0014', 'gpu0015', 'gpu0022', 'gpu0023', 'gpu0024', 'gpu0025'
+    ]
+    result = hosts.expand_hostlist('ng032,cn[304-306,308],gpu00[1-2][2-5]')
+    assert result == expected
